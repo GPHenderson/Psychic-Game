4c6fd91d09087ecb8d4aed424fc96686cbae0ff2
Gregory Henderson <gh_03@yahoo.com>
2019-08-11 22:29:43 -0500

    updated game.js file so that the game could function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,49 +3,43 @@ var computerChoice =['a','b','c','d','e','f','g', 'h', 'i', 'j', 'k', 'l', 'm', 
 //Variables for wins, losses, and gueses left
 var wins= 0;
 var losses = 0;
-var guesses = 10; 
+var guesses = 9;
 var guessesSofar= [];
+var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
 
+function guessCount(){
+    document.getElementById("guessesLeft-text").innerHTML = "Guesses Left : " + guesses;
+}
 
-var directionsText = document.getElementById("directions-text");
-var userChoiceText = document.getElementById("guessesSoFar-text");
-var guessesText = document.getElementById("computerChoice-text");
-var winsText = document.getElementById("wins-text");
-var lossesText = document.getElementById("losses-text");
-var guessesLeftText = document.getElementById("guessesLeft-text");
-var guessesSoFarText = document.getElementById("guessesSoFar-text");
+function userGuessSoFar(){
+    document.getElementById("guessesSoFar-text").innerHTML= "Your Guesses So Far :"+guessesSofar.join(' ');
+}
 
+guessCount();
 
+var restart = function () {
+    guesses = 9;
+    guessesSofar = [];
+    computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+}
 
-document.onkeyup = function (event) {
-    var userGuess=event.key;
+document.onkeyup = function() {
+    guesses--;
 
-    var computerGuess = computerChoice[Math.floor(Math.random() * computerChoice.length)];
+    var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+  
+    guessesSofar.push(userGuess);
+    if (userGuess === computerGuess) {
+        wins++;
+        document.getElementById("wins-text").innerHTML = "Wins : " + wins;
+        restart();
 
-    if (userGuess === computerChoice){
-        wins++;  
-        guessesLeftText= 10;
-        guessesSoFarText= [];
-    } 
-    if (userGuess !== computerChoice)
-    {guessesLeftText--;}
-
-    if (guessesLeftText==0) {
-      lossesText++;
-      guessesSoFarText = [];
-      guessesLeftText= 10;
+    } else if (guesses === 0) {
+        losses++;
+        document.getElementById("losses-text").innerHTML = "Loses : " + losses;
+        restart();
     }
 
-   if (guessesLeftText.indexOf (userGuess)>=0) {
-
-   }
-   else{guessesSoFarText.push (guessesSoFarText)
-    userChoiceText.textContent = "Your Guesses so far: ";
-
-        directionsText.textContent = "";
-
-        winsText.textContent = "Wins: " + wins;
-        lossesText.textContent = "Losses: " + losses;
-        gussesLeftText.textContent= "Guess Left: ";
-}
+    guessCount();
+    userGuessSoFar();
 };
